e8c69ec50cc973c402030920c0a229dfd40ad59a
ecstAsy <hexiaoqiang@cketrip.com>
2021-03-22 16:46:22 +0800

    update webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,17 @@
 const path = require('path');
-const NODE_ENV = process.env.NODE_ENV;
-const isProd = NODE_ENV === 'production';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const nodeExternals = require('webpack-node-externals');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const plugins = [
   new CleanWebpackPlugin(),
   new MiniCssExtractPlugin({
     filename: "[name].css",
     chunkFilename: "[id].css"
+  }),
+  new HtmlWebpackPlugin({
+    template: 'public/index.html'
   })
-];
+]
 
 const CssLoader = [
   MiniCssExtractPlugin.loader,
@@ -19,12 +19,12 @@ const CssLoader = [
 ]
 
 module.exports = {
-  mode: 'production',
-  entry: './src/components/index.js',
+  mode: 'development',
+  entry: './src/app.js',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, './dist'),
-    libraryTarget: 'commonjs2',  // 包需要被module.exports，这就要用到common
+    libraryTarget: undefined,  // 包需要被module.exports，这就要用到common
   },
   resolve: {
     // 要解析的文件的扩展名
@@ -62,6 +62,6 @@ module.exports = {
     port: 3000,
     open: true
   },
-  externals: isProd ? [nodeExternals()] : [], // nodeExternals 使得打包的组件中不包括任何 node_modules 里面的第三方组件，起到减小体积的作用。
+  externals: [], // nodeExternals 使得打包的组件中不包括任何 node_modules 里面的第三方组件，起到减小体积的作用。
   plugins,
 };
